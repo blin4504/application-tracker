a1d24d24ed2ee1dc1ead9f356ccb3780df53d629
blin4504 <bl4504@g.rit.edu>
2023-05-20 22:16:17 -0400

    allow for storing and reading of sqlite db

---
+++ main.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import sqlite3
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1rAsGas46T1xOOlNz97D02BGaBdqeBhxx-avSd6kluMg'
+RANGE_NAME = 'Sheet1!B:C'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                    range=RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+        
+        counter = 0
+        for v in values:
+            if v:
+                counter += 1
+
+        conn = sqlite3.connect('AA_db.sqlite')
+        cur = conn.cursor()
+        cur.execute(f'INSERT INTO applications (number) values ({counter})')
+        conn.commit()
+
+        cur.execute('SELECT * FROM applications')
+        data = cur.fetchall()
+
+        cur.execute('SELECT * FROM applications WHERE ID = (SELECT MAX(ID) FROM applications)')
+        data = cur.fetchall()
+
+        print(f"""
+            Total Applications: {len(data)}
+            Applications From Yesterday: {counter}
+            Total New Applications: {counter - data[0][2]}
+        """)
+        if counter - data[0][2] < 10:
+            print("APPLY MORE")
+
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
